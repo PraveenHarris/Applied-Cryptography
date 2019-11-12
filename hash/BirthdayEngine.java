2c5bf8136c1f2974e3fe7180139df68041d18fb7
Praveen David Harris <praveenharris@yahoo.com>
2019-11-12 11:41:13 -0500

    Add BirthdayEngineImplementation.java

---
+++ hash/BirthdayEngine.java
@@ -0,0 +1,80 @@
+package hash;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+
+public class BirthdayEngine {
+
+    private static BigInteger factorial(BigInteger from, BigInteger to) {
+        if (from.compareTo(to) < 0)
+            return BigInteger.ONE;
+
+        return from.multiply(factorial(from.subtract(BigInteger.ONE), to));
+    }
+
+    public static double computeActualProbability(int totalNumberOfDays, int nmberOfPeople) {
+        /*
+        Problem: Compute the probability of at least two people sharing the same birthdays
+        Idea: P(at_least_2) = P(at_least_2) + P(at_least_3) + ...
+        Easier way: P(at_least_2) = 1 - P(no_one_sharing_same_bday)
+         */
+
+        // parse variables to BigInteger
+        BigInteger noOfDays = new BigInteger(String.valueOf(totalNumberOfDays));
+        BigInteger noOfPeople = new BigInteger(String.valueOf(nmberOfPeople));
+
+        // calculate P_prime
+        BigInteger factorials = factorial(noOfDays, (noOfDays.subtract(noOfPeople)).add(BigInteger.ONE)); // +1 for the offset
+        BigDecimal p_prime_numerator = new BigDecimal(factorials);
+        BigDecimal p_prime_denominator = new BigDecimal((noOfDays).pow(nmberOfPeople));
+        double p_prime = p_prime_numerator.divide(p_prime_denominator, MathContext.DECIMAL32).doubleValue();
+
+        // actual probability
+        double p = 1 - p_prime;
+        return p;
+    }
+
+    public static double computeApproximation(int totalNumberOfDays, int numberOfPeople) {
+        double exponentOfE = (-1.0 * Math.pow(numberOfPeople, 2)) / (2.0 * totalNumberOfDays);
+        double approx = 1 - Math.pow(Math.E, exponentOfE);
+
+        return approx;
+    }
+
+    public static void main(String[] args) {
+
+        int totalNumberOfDays = 365;
+        int numberOfPeople = 50;
+
+        double actualProbability = BirthdayEngine.computeActualProbability(totalNumberOfDays, numberOfPeople);
+        System.out.println(actualProbability);
+
+        double approximatedProbability = BirthdayEngine.computeApproximation(totalNumberOfDays, numberOfPeople);
+        System.out.println(approximatedProbability);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
