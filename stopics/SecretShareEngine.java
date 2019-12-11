f2c3ce43f7c868df01a8f00c646c1199de6dc17d
Praveen David Harris <praveenharris@yahoo.com>
2019-12-10 23:22:39 -0500

    Update SecretShareEngine

--- stopics/SecretShareEngine.java
+++ stopics/SecretShareEngine.java
@@ -38,25 +38,4 @@ public class SecretShareEngine {
         return new SharesAndCoeff(shares, coeff);
     }
 
-
-    public static void main(String[] args) {
-//        int M = 44;
-//        int totalNumberOfShares = 5;
-//        int totalNeededToReconstitute = 3;
-//        SharesAndCoeff sharesAndCoeff = (new SecretShareEngine()).compute(totalNumberOfShares,totalNeededToReconstitute, M, new int[]{30, 50});
-//        int[][] shares = sharesAndCoeff.shares;
-//        int[] coeff = sharesAndCoeff.coeff;
-//
-//        System.out.println("M="+M);
-//        for (int i=0; i<coeff.length; i++) {
-//            System.out.printf("x^%d=%d\n", i+1, coeff[i]);
-//        }
-//        System.out.println();
-//
-//        for (int i=0; i<totalNumberOfShares; i++) {
-//            System.out.printf("(%d, %d)\n", shares[i][0], shares[i][1]);
-//        }
-    }
-
-
 }
