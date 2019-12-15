8b4df7ebaf739c922e8d3be0bea8a0311b4a86b9
Praveen David Harris <praveenharris@yahoo.com>
2019-12-14 20:11:48 -0500

    reorg

--- stopics/SecretShareEngine.java
+++ stopics/SecretShareEngine.java
@@ -1,6 +1,10 @@
 package stopics;
 
+import util.ArrayUtil;
+
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class SecretShareEngine {
 
@@ -14,28 +18,53 @@ public class SecretShareEngine {
         }
     }
 
-    public SharesAndCoeff compute(int totalNumberOfShares, int totalNeedToReconstitute, int M, int[] xRange) {
+    /**
+     * @param totalNumberOfShares number of shares requested
+     * @param totalNeedToReconstitute number of shares required to reconstitute the secret
+     * @param M secret
+     * @param xRange the range generated shares (used for bitStrength number of digits in the share)
+     * @return SharesAndCoeff object containing shares and coefficients for the secret
+     */
+    public SharesAndCoeff generateShares(int totalNumberOfShares, int totalNeedToReconstitute, int M, int modulus,  int[] xRange) {
         int from = xRange[0], to = xRange[1];
         int range = to - from;
         Random random = new Random();
+        modulus = modulus==0 ? 101 : modulus;
 
-        int[][] shares = new int[totalNumberOfShares][2];
+        int[][] shares = new int[totalNumberOfShares][2]; // (x,y)
         int noOfCoefficients = totalNeedToReconstitute - 1;
         int[] coeff = new int[noOfCoefficients];
         for (int i=0; i<noOfCoefficients; i++)
             coeff[i] = (int) (random.nextDouble() * 5) + 5;
+        int[] xCord = getDistinctNumbers(totalNumberOfShares, xRange[0], range);
 
         for (int i=0; i<totalNumberOfShares; i++) {
-            int x = (int) (random.nextDouble() * range) + from;
-            int y = M, degree=1;
+            int degree=1, y = M;
             for (int j=0; j<noOfCoefficients; j++) {
-                y += Math.pow(x, degree++) * coeff[j];
+                y += Math.pow(xCord[i], degree++) * coeff[j]; // y=..+bx^i+..
             }
-            shares[i][0] = x;
-            shares[i][1] = y;
+            shares[i][0] = xCord[i];
+            shares[i][1] = y % modulus;
         }
 
         return new SharesAndCoeff(shares, coeff);
+    }
+
+    public static int[] getDistinctNumbers(int total, int from, int range) {
+        Random random = new Random();
+        int[] rtn = new int[total];
+        HashSet<Integer> set = new HashSet<>();
+
+        while (set.size() != total) {
+            set.add((int) (random.nextDouble()*range) + from);
+        }
+
+        int i=0;
+        for (int x: set) {
+            rtn[i++] = x;
+        }
+
+        return rtn;
     }
 
 }
